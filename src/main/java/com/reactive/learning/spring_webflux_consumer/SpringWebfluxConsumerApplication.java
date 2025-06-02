16a60a845c84f1ecc9d2c52f2df5934d79fb1df9
Rahul Soni <rahulsoni.myventures@gmail.com>
2025-06-02 08:29:05 +0530

    initial commit

---
+++ src/main/java/com/reactive/learning/spring_webflux_consumer/SpringWebfluxConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.reactive.learning.spring_webflux_consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebfluxConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebfluxConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reactive/learning/spring_webflux_consumer/SpringWebfluxConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reactive.learning.spring_webflux_consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebfluxConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
